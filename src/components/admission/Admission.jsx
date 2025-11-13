baab70b44cb4eec0646b62b4aa72c3e287c07912
Sneha Agarwal <sneha050105@gmail.com>
2025-11-14 05:16:37 +0530

    Update Admission.jsx

--- src/components/admission/Admission.jsx
+++ src/components/admission/Admission.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './AdmissionPage.css';
+import './Admission.css';
 
 const Admission = () => {
   const navigate = useNavigate();
