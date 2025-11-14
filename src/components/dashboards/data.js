3544cf9f8fdcb3b916024a580287fd3664414b45
Sneha Das <snehadas2465@gmail.com>
2025-11-14 15:26:28 +0530

    Update data.js

--- src/components/dashboards/data.js
+++ src/components/dashboards/data.js
@@ -1,7 +1,8 @@
 export const users = [
   { id: "s001", password: "pass1", role: "student", name: "Sneha Agarwal" },
-  { id: "s002", password: "pass2", role: "student", name: "Rahul Mehta" },
-  { id: "s003", password: "pass3", role: "student", name: "Priya Sharma" },
+  { id: "s002", password: "pass2", role: "student", name: "Sneha Das" },
+  { id: "s003", password: "pass3", role: "student", name: "Arpayitri Bhattacharyya" },
+  { id: "s004", password: "pass4", role: "student", name: "Sakshi Agarwal" },
   { id: "admin", password: "admin123", role: "admin", name: "Administrator" },
   { id: "faculty01", password: "faculty123", role: "facultyReviewer", name: "facultyReviewer" },
   { id: "office01", password: "office123", role: "admissionOfficer", name: "admissionOfficer" },
