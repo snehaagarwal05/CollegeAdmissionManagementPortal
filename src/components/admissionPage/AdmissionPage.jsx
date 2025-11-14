a7c05285ffcc09c4d4bd22c7a65bb721e2e936c2
Sneha Das <snehadas2465@gmail.com>
2025-11-14 12:19:41 +0530

    Update AdmissionPage.jsx

--- src/components/admissionPage/AdmissionPage.jsx
+++ src/components/admissionPage/AdmissionPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './AdmissionForm.css';
+import './AdmissionPage.css';
 
 const AdmissionForm = () => {
   const navigate = useNavigate();
